eb7d5fb8981328535e6462eb8297d7da2ee4ea8c
zhangkun <zk870626!>
2017-09-06 16:38:38 +0800

    cg

---
+++ src/components/search/search.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="module">
+    <!--module content-->
+    搜索
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {name: `module`}
+</script>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
